35e381aca73a9ffef12c59367a1d788f532636c3
jeong <ibory1220@naver.com>
2022-04-05 06:48:23 +0900

    project set up & add firebase

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react/cjs/react.development";
+
+const LoginPage = (props) => {
+  return (
+    <Fragment>
+      <header></header>
+      <footer></footer>
+    </Fragment>
+  );
+};
+
+export default LoginPage;
